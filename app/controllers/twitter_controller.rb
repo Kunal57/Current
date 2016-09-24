03dba1d2b65f7a36d57996db001bd452345ae6cd
Kunal Patel <kunalpatelx92@gmail.com>
2016-09-24 16:17:59 -0500

    Add Bing Images API

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,12 +4,22 @@ class TwitterController < ApplicationController
   def index
     @trends_array = []
     twitter.trends(id = 23424977, options = {}).each{|trend| @trends_array<<trend.name}
-    @trends_array
+    puts @trends_array
+
+		BingSearch.account_key = '0WB5cSxqxRgAtwqLRqo1egZ6BubXFirjmJOMcIUSfdY'
+    @images_array = []
+    @trends_array.each do |trend|
+    	puts trend
+    	result = BingSearch.image(trend)
+    	if result[0] != nil
+				@images_array<<result[0].thumbnail.url
+			end
+    end
+    @images_array
   end
 
   def show
     @trends_array
   end
-
 
 end
