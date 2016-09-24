89181b716dc89b73b71c6d5a1acfa1cea6009b84
fwortham4 <forrest.wortham@gmail.com>
2016-09-23 19:58:49 -0500

    Creates basic structure for the Icon (show) Page.

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -1,11 +1,15 @@
 class TwitterController < ApplicationController
   include TwitterHelper
 
-def index
-  @trends_array = []
-  twitter.trends(id = 23424977, options = {}).each{|trend| @trends_array<<trend.name}
-  @trends_array
-end
+  def index
+    @trends_array = []
+    twitter.trends(id = 23424977, options = {}).each{|trend| @trends_array<<trend.name}
+    @trends_array
+  end
+
+  def show
+    @trends_array
+  end
 
 
 end
