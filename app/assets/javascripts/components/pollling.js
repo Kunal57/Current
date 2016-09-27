c1396f30a7755fba2d961f5e525901780e51e4a5
Kunal Patel <kunalpatelx92@gmail.com>
2016-09-27 14:49:28 -0500

    Uncomment Polling.js

--- app/assets/javascripts/components/pollling.js
+++ app/assets/javascripts/components/pollling.js
@@ -1,14 +1,14 @@
-// (function poll() {
-//   setTimeout(function() {
-//     $.ajax({
-//       url: "/trends/api",
-//       success: function(response) {
-//         console.log(response)
-//         poll()
-//         // update the dom
-//       }
-//     });
-//   }, 120000)})();
+(function poll() {
+  setTimeout(function() {
+    $.ajax({
+      url: "/trends/api",
+      success: function(response) {
+        console.log(response)
+        poll()
+        // update the dom
+      }
+    });
+  }, 120000)})();
 
 
 
